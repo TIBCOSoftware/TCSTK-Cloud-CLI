dae14c4ad659fc1710cb54687ecde25b896255a9
hpeters@tibco.com <hpeters@tibco.com>
2019-09-06 17:18:04 +0200

    Fix to delete distribution file before build (4)

--- build/project-functions.js
+++ build/project-functions.js
@@ -112,19 +112,22 @@ function cloudLoginV3(tenantID, clientID, email, pass, TCbaseURL) {
     return re;
 }
 
+cleanDist = function (){
+    return deleteFolder('./dist/' + propsF.App_Name);
+}
+
 // Function that builds the zip for deployment
 buildCloudStarterZip = function (cloudStarter) {
     return new Promise(function (resolve, reject) {
         const csURL = '/webresource/apps/' + cloudStarter + '/';
-        deleteFolder('./dist/' + cloudStarter).then( () => {
-                deleteFile('./dist/' + cloudStarter + '.zip');
-                run('ng build --prod --base-href ' + csURL + 'index.html --deploy-url ' + csURL);
-                //copyFile('./tmp/' + cloudStarter + '/tsconfig.build.json', './tmp/' + cloudStarter + '/tsconfig.json');
-                run('cd ./dist/' + cloudStarter + '/ && zip -r ./../' + cloudStarter + '.zip .');
-                log(INFO, 'ZIP Created: ./dist/' + cloudStarter + '.zip');
-                resolve();
-            }
-        );
+        // TODO: delete folder and then continue.
+        // deleteFolder('./dist/' + cloudStarter);
+        deleteFile('./dist/' + cloudStarter + '.zip');
+        run('ng build --prod --base-href ' + csURL + 'index.html --deploy-url ' + csURL);
+        //copyFile('./tmp/' + cloudStarter + '/tsconfig.build.json', './tmp/' + cloudStarter + '/tsconfig.json');
+        run('cd ./dist/' + cloudStarter + '/ && zip -r ./../' + cloudStarter + '.zip .');
+        log(INFO, 'ZIP Created: ./dist/' + cloudStarter + '.zip');
+        resolve();
     });
 }
 
