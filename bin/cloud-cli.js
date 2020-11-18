69846076159f9204eaa65c05b98fdd88a65095ca
hpeters@tibco.com <hpeters@tibco.com>
2020-11-18 16:14:55 +0100

    docs

--- bin/cloud-cli.js
+++ bin/cloud-cli.js
@@ -2,5 +2,6 @@
 global.TIME = new Date();
 global.SHOW_START_TIME = false;
 if(global.SHOW_START_TIME) console.log((new Date()).getTime() - global.TIME.getTime(), 'First ');
+// console.log('\x1b[35m%s\x1b[0m', 'TIBCO CLOUD CLI]', '\033[0m', 'V' + require('./../package.json').version);
 require = require('esm')(module);
 require('../cli').cli(process.argv);
