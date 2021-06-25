8cef48bbdb3e47cbdfd16db060d5429843f8e4a6
hpeters@tibco.com <hpeters@tibco.com>
2021-06-25 14:56:15 +0200

    Discover Datasets

---
+++ accessDiscover.js
@@ -0,0 +1,18 @@
+
+const fs = require('fs')
+
+function accessDiscover (useDiscover) {
+  const tasks = require('./src/config/config-cli-task.json').cliTasks
+  for (const tn in tasks) {
+    if (tasks[tn].category === 'discover') {
+      console.log(tn)
+      tasks[tn].enabled = useDiscover
+    }
+  }
+  const dataForFile = JSON.stringify({
+    cliTasks: tasks
+  })
+  fs.writeFileSync('./src/config/config-cli-task.json', dataForFile, 'utf8')
+}
+
+accessDiscover(false)
